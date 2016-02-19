dd665e94771792759b2a6b6a0126766a30a3dbf7
ProMike360 <mike.brouwer@edu-kw1c.nl>
2016-02-19 01:06:44 +0100

    Create Script.php
    Added the Script.php file, and uploaded the code.

---
+++ Script.php
@@ -0,0 +1,40 @@
+<?php
+
+    //Created by Mike Brouwer
+    //19-02-16
+
+    //CONNECT TO A SQL DATABASE USING MySQLi
+
+    //Database information
+    $SQLi_host      =   'localhost';
+    $SQLi_username  =   'username';
+    $SQLi_password  =   'password';
+    $SQLi_database  =   'database';
+    
+    //Connecting to the database
+    $SQLi_con       =   mysqli_connect($SQLi_host, $SQLi_username, $SQLi_password, $SQLi_database);
+
+    //Check connection
+    if ( mysqli_connect_errno() ) {
+        echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+    }
+
+    //Selection setting
+    $SQLi           =   'SELECT * FROM `table1` WHERE 1';
+    
+    //Run selection command
+    if ( $SQLi_result = mysqli_query($SQLi_con, $SQLi) ) {
+        // Fetch one and one row
+        while ( $SQLi_row = mysqli_fetch_row($SQLi_result) ) {
+            
+            //INSERT HERE YOUR CODE THAT WANTS TO USE DATA OUT OF THE DATABASE
+
+            print $SQLi_row[0];
+            print '<br>';
+        }
+        // Free result set
+        mysqli_free_result($SQLi_result);
+    }
+
+    mysqli_close($SQLi_con);
+?>
